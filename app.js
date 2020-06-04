a3acda4df5b36484b2a68b0603a427d99cacb74f
Matias Flores <arnod7991@gmail.com>
2020-06-04 15:40:59 -0400

    agregnado server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("hola mundo");
+});
+
+app.listen(3000, () => {
+  console.log('Escuchando el puerto 3000');
+})
+
 console.log('hola mundo');
